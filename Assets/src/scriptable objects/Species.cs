2d884dc9c1c03034419d2b454e84f87b58277f05
Im-Stivik <mogil.sharon@gmail.com>
2023-09-03 18:32:10 +0300

    made the species_info.h file

---
+++ Assets/src/scriptable objects/Species.cs
@@ -0,0 +1,45 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Species", menuName = "Pokemon Species", order = 0)]
+public class Species : ScriptableObject {
+
+    [SerializeField]private ushort baseHP;
+    [SerializeField] private ushort baseAttack;
+    [SerializeField] private ushort baseDefence;
+    [SerializeField] private ushort spAttack;
+    [SerializeField] private ushort spDeffence;
+    [SerializeField] private ushort speed;
+
+    [SerializeField] private Type primaryType;
+    [SerializeField] private Type seconderyType;
+
+    [SerializeField] private ushort catchRate;
+    [SerializeField] private ushort expYield;
+    
+    [SerializeField]private ushort evYield_HP;
+    [SerializeField] private ushort evYield_Attack;
+    [SerializeField] private ushort evYield_Defence;
+    [SerializeField] private ushort evYield_spAttack;
+    [SerializeField] private ushort evYield_spDeffence;
+    [SerializeField] private ushort evYield_speed;
+
+    [SerializeField] private Item itemCommon;
+    [SerializeField] private Item itemRare;
+
+    [SerializeField] private bool genderless;
+    [SerializeField] private float genderRatio; //precet of female.
+    
+    [SerializeField] private ushort eggCycles;
+    [SerializeField] private ushort friendship;
+
+    [SerializeField] private GrouthRate grouthRate;
+    [SerializeField] private EggGroup[] eggGroups;
+
+    [SerializeField] private Ability[] abilities;
+    [SerializeField] private ushort safariZoneFleeRate;
+    [SerializeField] private BodyColor bodyColor;
+    [SerializeField] private bool noFlip;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Pokemon-Gen-3-Port
  this is a reverse engeniring for pokemon FRLG to all platforms
+ 
+# Warning: this may include copyrighted material associated with GameFreak, Nintendo and The Pokemon Company
+
+this project aims to recreated the Pokemon FRLG games on unity for easy use and modding.
+
+this project relaying on the reverse engeniring fire red and leaf green project from: 
+https://github.com/pret/pokefirered
+
+# current Progress on files recreated:
+    * species_info.h - Done
